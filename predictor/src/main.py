367577293b3b47a1c996e4ff8d58e43c139c2204
Pierre PLAZANET <plazanet.pierre@gmail.com>
2019-12-12 10:48:01 +0100

    Implement tutorial

---
+++ predictor/src/main.py
@@ -0,0 +1,48 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy as np
+
+import tensorflow as tf
+
+import tensorflow_hub as hub
+import tensorflow_datasets as tfds
+
+print("Version: ", tf.__version__)
+print("Eager mode: ", tf.executing_eagerly())
+print("Hub version: ", hub.__version__)
+print("GPU is", "available" if tf.config.experimental.list_physical_devices("GPU") else "NOT AVAILABLE")
+
+train_validation_split = tfds.Split.TRAIN.subsplit([6, 4])
+
+(train_data, validation_data), test_data = tfds.load(
+    name="imdb_reviews", 
+    split=(train_validation_split, tfds.Split.TEST),
+    as_supervised=True)
+
+train_examples_batch, train_labels_batch = next(iter(train_data.batch(10)))
+
+embedding = "https://tfhub.dev/google/tf2-preview/gnews-swivel-20dim/1"
+hub_layer = hub.KerasLayer(embedding, input_shape=[], 
+                           dtype=tf.string, trainable=True)
+hub_layer(train_examples_batch[:3])
+
+model = tf.keras.Sequential()
+model.add(hub_layer)
+model.add(tf.keras.layers.Dense(16, activation='relu'))
+model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+
+model.summary()
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit(train_data.shuffle(10000).batch(512),
+                    epochs=20,
+                    validation_data=validation_data.batch(512),
+                    verbose=1)
+
+results = model.evaluate(test_data.batch(512), verbose=2)
+
+for name, value in zip(model.metrics_names, results):
+    print("%s: %.3f" % (name, value))
